2181702d56327ba7aa252bc45ddade771d672e3b
Oleg Pavlov <oleg.pavlov@aol.com>
2017-12-05 00:09:10 +0300

    Initial commit

---
+++ src/main/java/tech/letscode/rpsgame/RpsGame.java
@@ -0,0 +1,12 @@
+package tech.letscode.rpsgame;
+
+/**
+ * Oleg Pavlov <oleg.pavlov@aol.com>
+ */
+public class RpsGame
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
