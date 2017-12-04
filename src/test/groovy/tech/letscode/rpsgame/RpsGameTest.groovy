9db1333641285fb581fac186440e7e8292d65444
Oleg Pavlov <oleg.pavlov@aol.com>
2017-12-05 00:23:10 +0300

    Add infrastructure

---
+++ src/test/groovy/tech/letscode/rpsgame/RpsGameTest.groovy
@@ -0,0 +1,14 @@
+package tech.letscode.rpsgame
+
+import spock.lang.Specification
+
+/**
+ * Oleg Pavlov <oleg.pavlov@aol.com>
+ */
+class RpsGameTest extends Specification
+{
+    def "empty test case"() {
+        //empty
+        true
+    }
+}
