ce629c5908cce22d620d74948df7e95595e6dd08
Oleg Pavlov <oleg.pavlov@aol.com>
2017-12-07 23:58:38 +0300

    Fix links in javadoc.

--- src/main/java/tech/letscode/rpsgame/domain/model/Game.java
+++ src/main/java/tech/letscode/rpsgame/domain/model/Game.java
@@ -5,8 +5,8 @@ import tech.letscode.rpsgame.shared.Args;
 import javax.annotation.Nonnull;
 
 /**
- * This class models Rock-Paper-Scissor game. To play this game you need to pass two players and call @{Game#play}
- * method and pass there @{Outcome} callback to be notified about the result.
+ * This class models Rock-Paper-Scissor game. To play this game you need to pass two players and call {@link Game#play(Outcome)}
+ * method and pass there {@link Outcome} callback to be notified about the result.
  * Mechanics. Each of the players is asked to choose one of the shapes (Rock, Paper, Scissor) and after that,
  * the winner will be computed.
  * For example. If the first player decided to play rock and the second player decided to play paper then
@@ -14,6 +14,7 @@ import javax.annotation.Nonnull;
  * <p>
  * Full description of the game you can find here https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
  * <p>
+ *
  * Oleg Pavlov <oleg.pavlov@aol.com>
  */
 public class Game
@@ -32,7 +33,7 @@ public class Game
      * Runs the game.
      *
      * @param outcome Callback to notify a client about the result. Only one of the methods will be called.
-     * @throws IllegalArgumentException if outcome is null or @{Player#choose} returns null.
+     * @throws IllegalArgumentException if outcome is null or {@link Player#choose()} returns null.
      */
     public void play(@Nonnull Outcome outcome)
     {
